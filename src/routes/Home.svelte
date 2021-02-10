5ddbba944301cfb6c45c1f543933bcc23b366e4b
Lazaro Cubas <68677716+lazgps@users.noreply.github.com>
2021-02-10 21:07:54 +0000

    fix home page responsive

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,6 +1,6 @@
 <section class="text-gray-600 w-screen z-1 container">
-    <div class="bg-gray-100 flex flex-row items-left justify-between">
-        <div class="bg-white bg-opacity-50 rounded-lg my-10 mx-4 py-10 px-4 w-2/6">
+    <div class="bg-gray-100 flex flex-col items-left content-start justify-start">
+        <div class="bg-white bg-opacity-50 rounded-lg mr-auto my-5 py-5 md:pl-10 w-4/6 md:w-2/6">
           <h1 class="text-4xl text-left uppercase text-red-500 font-bold title-font w-4/5 ml-8">Global Property Services Realty & Property Management</h1>
           <br>
           <h2 class="text-lg text-gray-500 text-left w-4/5 ml-8">Whether you want to find a place to live or need a rental filled, when you are here, you're home</h2>
@@ -9,20 +9,20 @@
           </button>
         </div>
       </div>
-    <div class="container flex flex-row items-center mx-auto px-80 py-24 md:flex-row">
-      <div class="flex flex-col items-center text-center lg:flex-grow lg:pr-24 md:w-1/2 md:pr-16 md:items-start md:text-left mb-16 md:mb-0">
+    <div class="container flex flex-col md:flex-row items-center mx-auto px-auto py-16 md:py-16 w-4/5">
+      <div class="flex flex-col items-center text-center lg:flex-grow lg:pr-24 md:pr-16 w-1/2 md:items-start md:text-left md:px-16">
         <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">No More running after manager</h1>
         <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
       </div>
-      <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+      <div class="flex-grow lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
         <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
       </div>
     </div>
-    <div class="container mx-auto flex px-80 py-24 md:flex-row flex-col items-center">
-        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
+    <div class="container flex flex-col items-center md:flex-row mx-auto px-auto py-16 md:py-16 w-4/5">
+        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
           <img class="object-cover object-center rounded" alt="hero" src="https://dummyimage.com/720x600">
         </div>
-        <div class="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
+        <div class="flex flex-col items-center text-center lg:flex-grow lg:pl-24  md:pl-16 w-1/2 md:items-start md:text-left">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">No Funny Business
           </h1>
           <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
